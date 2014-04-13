e0c91ebfd3b75301e055d0e5fe640bf0b689b213
Anb <benoit@anbcorp.net>
2014-04-13 15:45:56 +0200

    Simple working demo, tiles are not handled yet

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 __author__ = 'benoit'
 
 import pygame
+import resources
 
 class Player(pygame.sprite.Sprite):
 
@@ -9,10 +10,10 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(resources.getImage('player'))
         self.rect = pygame.rect.Rect(resources.getValue('player.start'), self.image.get_size())
         self.h_speed = resources.getValue('player.speed')
-        self.h_speed = resources.getValue('player.speed')
+        self.v_speed = resources.getValue('player.speed')
 
     def update(self, dt, game):
-        lase = self.rect.copy()
+        last = self.rect.copy()
 
         key = pygame.key.get_pressed()
 
@@ -24,3 +25,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.y -= self.v_speed * dt
         if key[pygame.K_DOWN]:
             self.rect.y += self.h_speed * dt
+
+        for cell in pygame.sprite.spritecollide(self, game.walls, False):
+            self.rect = last
+
